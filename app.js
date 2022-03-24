a5bbda700a727787546d7055e3a934bcaa1025e7
Niloy-Majumder <niloymajumder.98@gmail.com>
2022-03-24 19:23:53 +0530

    First Commit.Level 1 Auth.Basic storing and loging

---
+++ app.js
@@ -0,0 +1,66 @@
+//jshint esversion:6
+
+const express = require("express");
+const ejs= require("ejs");
+const mongoose = require("mongoose");
+
+const app= express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+
+const userSchema= new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User= new mongoose.model("User",userSchema);
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+})
+
+app.get("/register", (req,res)=>{
+    res.render("register");
+})
+
+
+app.post("/register",(req,res)=>{
+    const newUser= new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save();
+    res.render("secrets");
+})
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username;
+    const pass=req.body.password;
+    User.findOne({email:username},(err,foundUser)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            if(foundUser){
+                if (foundUser.password==pass) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server Running");
+})
